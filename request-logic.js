2d2be1340be67d44d5b75bfd9d6c9fff44299ba7
Staples71 <49735990+Staples71@users.noreply.github.com>
2019-05-05 18:49:08 -0700

    Add files via upload

---
+++ request-logic.js
@@ -0,0 +1,27 @@
+// We at Content Creators know this code is useful for getting the
+// extension off of the supplied filename, but we can't figure out the rest of
+// the function to use it! We hope this is useful to you!
+
+
+function getContentType(filename) {
+  const extension = filename.match(/.*\.([^\.]*)$/)[1];
+  /*instead of creating individual if statements, I created a single switch
+  statement and ran through the errors in mocha. Default being 'text/plain'*/
+  switch (extension){
+  case 'html':
+    return 'text/html';
+    break;
+  case 'css':
+    return 'text/css'
+    break;
+  case 'jpeg':
+    return 'image/jpeg';
+    break;
+  case 'jpg':
+    return 'image/jpeg';
+    break;
+  default:
+    return 'text/plain'
+    break;
+  }
+}
